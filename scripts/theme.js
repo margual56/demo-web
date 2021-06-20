6abe1fe91477377c8cc42eebbf01bd86fa56c17c
Marcos Gutiérrez Alonso <margual56@gmail.com>
2021-06-20 13:51:23 +0200

    Update to the posts, profile page and replies

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,8 +1,6 @@
 function changetheme(button){
     let goto_dark = button.id=="moon";
 
-    console.log(goto_dark);
-
     button.className = "theme";
     document.getElementById(goto_dark?"sun":"moon").className = "theme active";
 
