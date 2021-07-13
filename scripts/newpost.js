39a023f15a51f31dfef2c0dee8222d012ebdbf1f
Marcos Gutiérrez Alonso <margual56@gmail.com>
2021-07-13 19:14:28 +0200

    Improvements

--- scripts/newpost.js
+++ scripts/newpost.js
@@ -4,10 +4,16 @@ function newpost(button){
 	document.querySelector(".new-post-form").classList.add("active");
 
 	document.querySelectorAll("nav, main.content").forEach(elem => {
-		if(!elem.classList.contains("blurry")){
-			elem.classList.add("blurry");
-		}
+		elem.classList.toggle("blurry", true);
 	});
+}
 
+function hideNewpost(){
+	document.getElementById("new-post-button").style="";
+	
+	document.querySelector(".new-post-form").classList.remove("active");
 
+	document.querySelectorAll("nav, main.content").forEach(elem => {
+		elem.classList.toggle("blurry", false);
+	});
 }
